e36ed341a6873947d2d16bf216ebd6af45105fbd
Satyam <satyambhardawaj@gmail.com>
2020-09-09 12:36:21 +0530

    project initiation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //BEM naming convention
+
+    return(
+        <div className="app">
+            {/* <h1>Welcome to Whatsapp</h1> */}
+            <div className="app__body">
+                {/* Sidebar */}
+                <Sidebar />
+                {/* chat */}
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default App
---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBIiHTkomf91tgOeu9ua-vDpUV1gmgYubw",
+    authDomain: "whatsapp-1072c.firebaseapp.com",
+    databaseURL: "https://whatsapp-1072c.firebaseio.com",
+    projectId: "whatsapp-1072c",
+    storageBucket: "whatsapp-1072c.appspot.com",
+    messagingSenderId: "1007796474628",
+    appId: "1:1007796474628:web:41b7e391034a7c82343651",
+    measurementId: "G-W6XDQJ19GW"
+  };
