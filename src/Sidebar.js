1d5ad8e2776f4532dbcb5211716a309eb4c5b701
Satyam <satyambhardawaj@gmail.com>
2020-09-10 15:09:03 +0530

    sidebar almost complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Sidebar.css';
-import { Avatar } from '@material-ui/core';
+import SidebarChat from './SidebarChat';
+import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons';
+
 
 function Sidebar() {
     return (
@@ -11,13 +14,23 @@ function Sidebar() {
             <div className="sidebar__header">
                 <Avatar />
                 <div className="sidebar__headerRight">
-                    <DonutLargeIcon />
-                    <ChatIcon />
-                    <MoreVertIcon />
+                    <IconButton><DonutLargeIcon /></IconButton>
+                    <IconButton><ChatIcon /></IconButton>
+                    <IconButton><MoreVertIcon /></IconButton>
                 </div>
             </div>
-            <div className="sidebar__search"></div>
-            <div className="sidebar__chats"></div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start a new chat" type="text" />
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
         </div>
     )
 }
